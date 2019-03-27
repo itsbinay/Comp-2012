f0563de9e2682b772b1f003f98068d2dff4921ce
Binay Gurung <binaygurung9@gmail.com>
2019-03-27 22:54:24 +0800

    Testing out some examples from the lecture note

---
+++ 2012 Lecture notes example/Inheritance and Polymorphism example/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class Base{
+public:
+  Base(){cout<<"Base's constructor \n";}
+  virtual ~Base(){cout<<"Base's destructor \n";}
+};
+
+class Derived:public Base{
+public:
+  Derived(){cout<<"Derived's constructor \n";}
+  ~Derived(){cout<<"Derived's destructor \n";}
+};
+
+int main(){
+  Base *p=new Derived;
+  delete p;
+}
